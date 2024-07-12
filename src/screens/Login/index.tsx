f10ed1f8449fd40f12da9f3f53acb84794cd9e34
EulerLagrange.eth <hgpatel715@gmail.com>
2024-07-12 18:37:35 +0200

    expo

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,25 +1,25 @@
-import {NavigationProp} from '@react-navigation/native';
-import parseUrl from 'parse-url';
-import React, {useEffect} from 'react';
-import {Pressable, Text, View} from 'react-native';
-import WebView from 'react-native-webview';
-import {useRecoilState, useRecoilValue, useSetRecoilState} from 'recoil';
+import { NavigationProp } from "@react-navigation/native";
+import parseUrl from "parse-url";
+import React, { useEffect } from "react";
+import { Pressable, Text, View } from "react-native";
+import WebView from "react-native-webview";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import {
   codeState,
   randomDeviceIdState,
   randomStateState,
   tokenState,
-} from '../../atoms';
-import {submitCode} from '../../doordash';
-import {NavParams} from '../../navigation';
-import {generateDoordashLoginUrl} from '../../utils';
+} from "../../atoms";
+import { submitCode } from "../../doordash";
+import { NavParams } from "../../navigation";
+import { generateDoordashLoginUrl } from "../../utils";
 
 interface Props {
-  navigation: NavigationProp<NavParams, 'Profile'>;
+  navigation: NavigationProp<NavParams, "Profile">;
 }
 
-export const Login = ({navigation}: Props) => {
-  const {goBack} = navigation;
+export const Login = ({ navigation }: Props) => {
+  const { goBack } = navigation;
   const deviceId = useRecoilValue(randomDeviceIdState);
   const state = useRecoilValue(randomStateState);
   const [code, setCode] = useRecoilState(codeState);
@@ -28,8 +28,8 @@ export const Login = ({navigation}: Props) => {
 
   useEffect(() => {
     if (code) {
-      submitCode(code).then(resp => {
-        console.log('token', resp.token.token);
+      submitCode(code).then((resp) => {
+        console.log("token", resp.token.token);
         setToken(resp.token.token);
         goBack();
       });
@@ -43,7 +43,8 @@ export const Login = ({navigation}: Props) => {
         <Text className="text-white">Doordash Login</Text>
         <Pressable
           className="h-8 w-8 items-end justify-center"
-          onPress={goBack}>
+          onPress={goBack}
+        >
           <Text className="text-xs text-gray-200">Close</Text>
         </Pressable>
       </View>
@@ -53,8 +54,10 @@ export const Login = ({navigation}: Props) => {
         scalesPageToFit={false}
         // Remove incognito to preserve cache and restore login between sessions
         incognito={false}
-        userAgent={"Mozilla/5.0 (iPhone; CPU iPhone OS 17_1_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.1.1 Mobile/15E148 Safari/604.1"}
-        source={{uri: generateDoordashLoginUrl(deviceId, state)}}
+        userAgent={
+          "Mozilla/5.0 (Linux; Android 14; Pixel 8 Pro Build/AP1A.240305.019.A1; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/125.0.6422.3 Mobile Safari/537.36"
+        }
+        source={{ uri: generateDoordashLoginUrl(deviceId, state) }}
         // onNavigationStateChange={(navState) => {
         //   console.log('navState', navState);
         // if (navState.url.startsWith('ios-identity-framework://')) {
@@ -65,11 +68,11 @@ export const Login = ({navigation}: Props) => {
         //   return false;
         // }
         // }}
-        onShouldStartLoadWithRequest={req => {
-          console.log('request', req.url);
+        onShouldStartLoadWithRequest={(req) => {
+          console.log("request", req.url);
 
-          if (req.url.startsWith('ios-identity-framework://')) {
-            console.log('request', req.url);
+          if (req.url.startsWith("ios-identity-framework://")) {
+            console.log("request", req.url);
             const parsedUrl = parseUrl(req.url);
             const code = parsedUrl.query.code;
             setCode(code);
